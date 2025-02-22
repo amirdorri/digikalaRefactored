170a4c783258265e1ff4358aff9f054eb7234ef8
Amir <amirdorri81@gmail.com>
2025-02-22 21:13:31 +0330

    Continue buying added to cart section (UI)

---
+++ app/src/main/java/com/example/digikala/ui/screens/basket/BuyingProcessContinue.kt
@@ -0,0 +1,94 @@
+package com.example.digikala.ui.screens.basket
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.example.digikala.R
+import com.example.digikala.ui.theme.darkText
+import com.example.digikala.ui.theme.digikalaRed
+import com.example.digikala.ui.theme.elevation
+import com.example.digikala.ui.theme.roundedShape
+import com.example.digikala.ui.theme.spacing
+import com.example.digikala.util.DigitHelper
+
+@Composable
+fun BuyingProcessContinue(price: Long) {
+
+    Card(
+        shape = MaterialTheme.roundedShape.extraSmall,
+        elevation = MaterialTheme.elevation.extraSmall,
+        border = BorderStroke(color = Color.LightGray.copy(0.2f), width = 1.dp)
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = MaterialTheme.spacing.medium,
+                    vertical = MaterialTheme.spacing.semiMedium
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.digikalaRed),
+                shape = MaterialTheme.roundedShape.small
+            ) {
+                Text(
+                    text = stringResource(R.string.purchase_process),
+                    color = Color.White,
+                    style = MaterialTheme.typography.h5,
+                    modifier = Modifier
+                        .padding(
+                            horizontal = MaterialTheme.spacing.biggerMedium,
+                            vertical = MaterialTheme.spacing.extraSmall,
+                        )
+                )
+            }
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.goods_total_price),
+                    color = MaterialTheme.colors.darkText,
+                    style = MaterialTheme.typography.h6,
+                )
+                Row() {
+                    Text(
+                        text = DigitHelper.digitBytLocateAndSeparator(price.toString()),
+                        style = MaterialTheme.typography.body2,
+                        fontWeight = FontWeight.SemiBold
+                    )
+
+                    Image(
+                        painter = painterResource(id = R.drawable.toman),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(MaterialTheme.spacing.semiLarge)
+                            .padding(horizontal = MaterialTheme.spacing.extraSmall)
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/digikala/ui/screens/basket/ShoppingCart.kt
+++ app/src/main/java/com/example/digikala/ui/screens/basket/ShoppingCart.kt
@@ -2,22 +2,21 @@ package com.example.digikala.ui.screens.basket
 
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -28,11 +27,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.digikala.R
 import com.example.digikala.data.model.basket.CartItem
 import com.example.digikala.data.model.basket.CartStatus
-import com.example.digikala.ui.components.MyLoading
 import com.example.digikala.ui.theme.darkText
 import com.example.digikala.ui.theme.spacing
 import com.example.digikala.viewmodel.BasketViewModel
-import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun ShoppingCart(
@@ -42,86 +39,68 @@ fun ShoppingCart(
     val currentCartItemState: BasketScreenState<List<CartItem>> by
     viewModel.currentCartItems.collectAsState(BasketScreenState.Loading)
 
-    LazyColumn(
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(bottom = 60.dp),
-    ) {
-        when (currentCartItemState) {
-            is BasketScreenState.Success -> {
-                if ((currentCartItemState as BasketScreenState.Success<List<CartItem>>).data.isEmpty()) {
-                    item { EmptyBasket() }
-                    item { SuggestListSection() }
-                } else {
-                    items((currentCartItemState as BasketScreenState.Success<List<CartItem>>).data) { item ->
-                        CartItemCard(item, CartStatus.CURRENT_CART)
+            .fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ){
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(bottom = 60.dp),
+        ) {
+            when (currentCartItemState) {
+                is BasketScreenState.Success -> {
+                    if ((currentCartItemState as BasketScreenState.Success<List<CartItem>>).data.isEmpty()) {
+                        item { EmptyBasket() }
+                        item { SuggestListSection() }
+                    } else {
+                        items((currentCartItemState as BasketScreenState.Success<List<CartItem>>).data) { item ->
+                            CartItemCard(item, CartStatus.CURRENT_CART)
+                        }
+                        item {
+                            CartPriceDetailSection(cartDetail.value)
+                        }
                     }
+                }
+
+                is BasketScreenState.Loading -> {
                     item {
-                        CartPriceDetailSection(cartDetail.value)
+                        Column(
+                            modifier = Modifier
+                                .height(LocalConfiguration.current.screenHeightDp.dp - 60.dp)
+                                .fillMaxWidth()
+                                .padding(vertical = MaterialTheme.spacing.small),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = stringResource(R.string.please_wait),
+                                fontWeight = FontWeight.Bold,
+                                style = MaterialTheme.typography.h5,
+                                color = MaterialTheme.colors.darkText,
+                            )
+                        }
                     }
                 }
-            }
 
-            is BasketScreenState.Loading -> {
-                item {
-                    Column(
-                        modifier = Modifier
-                            .height(LocalConfiguration.current.screenHeightDp.dp - 60.dp)
-                            .fillMaxWidth()
-                            .padding(vertical = MaterialTheme.spacing.small),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = stringResource(R.string.please_wait),
-                            fontWeight = FontWeight.Bold,
-                            style = MaterialTheme.typography.h5,
-                            color = MaterialTheme.colors.darkText,
-                        )
-                    }
+                is BasketScreenState.Error -> {
+                    Log.e("3636", "err")
                 }
             }
+        }
 
-            is BasketScreenState.Error -> {
-                Log.e("3636", "err")
-            }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 60.dp)
+        ) {
+            BuyingProcessContinue(cartDetail.value.payablePrice)
         }
     }
 
 
-//    LazyColumn(
-//        modifier = Modifier
-//            .fillMaxWidth()
-//            .wrapContentHeight()
-//            .padding(bottom = 60.dp)
-//    ) {
-//
-//        when (currentCartItemState) {
-//
-//            is BasketScreenState.Success -> {
-//                if (currentCartItems.value.isEmpty()) {
-//                    item { EmptyBasket() }
-//                    item { SuggestListSection() }
-//                } else {
-//
-//                    items(currentCartItems.value) { item ->
-//                        CartItemCard(item, CartStatus.CURRENT_CART)
-//                    }
-//                }
-//            }
-//
-//            is BasketScreenState.Error -> {
-//
-//            }
-//
-//            is BasketScreenState.Loading -> {
-//               item {  Text( "this is loading", fontSize = 32.dp) }
-//            }
-//        }
-//
-//
-//
-//
-//    }
+
 }
